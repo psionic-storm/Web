c5aca811da2f4e06dee46fd65ff956655de4f8e6
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-03 04:15:40 +0900

    env : add eslint plugins and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
-export const env = {
-  browser: true,
-  node: true,
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 0,
+  },
 };
