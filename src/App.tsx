fa615877f1b039f767fa682827558e794b3c3482
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-03 18:52:49 +0900

    env : add path alias of tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import GlobalStyle from 'Styles/GlobalStyle';
-import { a } from 'Redux/1';
 
 function App() {
-  console.log(a);
   return (
     <>
       <GlobalStyle />
