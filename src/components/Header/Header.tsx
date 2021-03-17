e06d7216acfa53652f3aaf62a33b088e837f5d56
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-17 21:38:16 +0900

    feat : add HeaderBlock

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import HeaderBlock from 'Elements/HeaderBlock/HeaderBlock';
 import { Link } from 'react-router-dom';
 import * as S from './HeaderStyle';
 
 function Header() {
   return (
-    <S.Container>
-      <Link to="/">
-        <S.Logo>PsionicStorm</S.Logo>
-      </Link>
-      <Link to="/signIn">
-        <S.SignInButton>Sign In</S.SignInButton>
-      </Link>
-    </S.Container>
+    <>
+      <S.Container>
+        <Link to="/">
+          <S.Logo>PsionicStorm</S.Logo>
+        </Link>
+        <Link to="/signIn">
+          <S.SignInButton>Sign In</S.SignInButton>
+        </Link>
+      </S.Container>
+      <HeaderBlock />
+    </>
   );
 }
 
---
+++ src/elements/HeaderBlock/HeaderBlock.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './HeaderBlockStyle';
+
+function HeaderBlock() {
+  return (
+    <S.Container>
+    </S.Container>
+  );
+}
+
+export default HeaderBlock;
+
