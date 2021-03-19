d79225f5a89e959d567bf142af9032175d1faefd
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-19 14:41:25 +0900

    feat : set up redux store and root saga

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from 'Redux';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
