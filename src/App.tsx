eb41722096d05b16c4a5f36b432ac8e8caa6f545
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-20 16:56:17 +0900

    feat : add signIn api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react';
 import GlobalStyle from 'Styles/GlobalStyle';
 import AppRoutes from 'Routes/AppRoutes';
-import { signUp } from 'Apis/userAPI';
+import { getCurrentUser, signIn } from 'Apis/userAPI';
 
 function App() {
   const body = {
-    loginId: 'asdadf',
+    loginId: 'asdddaddf',
     password: 'gggg',
-    nickname: 'assss',
   };
 
   useEffect(() => {
-    const a = signUp(body);
+    const a = getCurrentUser(
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDIsI…DI0fQ.kll4uK8V3T6_VWch6A0yzGap9avoAi2KAkI2w1cRbaw',
+    );
     console.log(a);
   });
 
--- src/apis/userAPI.ts
+++ src/apis/userAPI.ts
@@ -3,10 +3,14 @@ import { psionicStorm, psionicStormAuth } from './base';
 export async function signUp({ loginId, password, nickname }: any) {
   const { data } = await psionicStorm.post('/user/signUp', { loginId, password, nickname });
 
-  console.log(data + 3);
   return data;
 }
 
+export async function signIn({ loginId, password }: any) {
+  const { data } = await psionicStorm.post('user/signIn', { loginId, password });
+  console.log(data);
+}
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDIsI…DI0fQ.kll4uK8V3T6_VWch6A0yzGap9avoAi2KAkI2w1cRbaw
 export async function getCurrentUser(token: string) {
   const { data } = await psionicStormAuth(token).get('/user');
 
