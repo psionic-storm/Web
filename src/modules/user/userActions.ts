faf0a058af06e24f5a0ec3db9f1b6f4483fe0499
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-21 15:56:38 +0900

    feat : add requestSignIn action

--- src/modules/user/userActions.ts
+++ src/modules/user/userActions.ts
@@ -10,6 +10,7 @@ export type RequestSignUpAction = ReturnType<typeof requestSignUp>;
 export type RequestSignUpActionPayload = { loginId: string; password: string; nickname: string };
 export type RequestSignUpSuccessActionPayload = number;
 export type RequestSignUpFailureActionPayload = Error;
+export type RequestSignInActionPayload = { loginId: string; password: string };
 
 export function requestSignUp({ loginId, password, nickname }: RequestSignUpActionPayload) {
   return <const>{
@@ -33,5 +34,15 @@ export function requestSignUpFailure(error: RequestSignUpFailureActionPayload) {
   return <const>{
     type: ActionTypes.REQUEST_SIGN_UP_FAILURE,
     payload: { error },
+  };
+}
+
+export function requestSignIn({ loginId, password }: RequestSignInActionPayload) {
+  return <const>{
+    type: ActionTypes.REQUEST_SIGN_IN,
+    payload: {
+      loginId,
+      password,
+    },
   };
 }
