402a6d43e973dca0c3c244289695a31c5edcffcd
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-21 22:06:14 +0900

    feat : add spaceReducer

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -5,13 +5,13 @@ import userReducer from './user/userReducer';
 import userSaga from './user/userSaga';
 import squareSaga from './square/squareSaga';
 import squareReducer from './square/squareReducer';
-import spaceSaga from './user/spaceSaga';
-import spaceReducer from './user/spaceReducer';
+import spaceSaga from './space/spaceSaga';
+import spaceReducer from './space/spaceReducer';
 
 const rootReducer = combineReducers({ modalReducer, userReducer, squareReducer, spaceReducer });
 
 export function* rootSaga() {
-  yield all([userSaga(), squareSaga()], spaceSaga());
+  yield all([userSaga(), squareSaga(), spaceSaga()]);
 }
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/modules/space/spaceReducer.ts
@@ -0,0 +1,198 @@
+import { Book } from 'Types/book';
+import { Comment } from 'Types/comment';
+import { Space } from 'Types/space';
+import { SpaceAction } from './spaceActions';
+import ActionTypes from './spaceActionTypes';
+
+interface SpaceState {
+  error?: Error;
+  space?: Space;
+  book?: Book;
+  comments?: Comment[];
+}
+
+const initialState = {};
+
+function spaceReducer(state: SpaceState = initialState, action: SpaceAction): SpaceState {
+  switch (action.type) {
+    case ActionTypes.REQUEST_GET_SPACE_SUCCESS:
+      return {
+        ...state,
+        space: action.payload,
+      };
+    case ActionTypes.REQUEST_GET_SPACE_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_UPDATE_SPACE_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_UPDATE_SPACE_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_GET_BOOK_SUCCESS:
+      return {
+        ...state,
+        book: action.payload,
+      };
+    case ActionTypes.REQUEST_GET_BOOK_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_ADD_BOOK_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_ADD_BOOK_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_DELETE_BOOK_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_DELETE_BOOK_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_ADD_REVIEW_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_ADD_REVIEW_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_UPDATE_REVIEW_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_UPDATE_REVIEW_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_DELETE_REVIEW_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_DELETE_REVIEW_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_GET_ALL_REVIEW_COMMENTS_SUCCESS:
+      return {
+        ...state,
+        comments: action.payload,
+      };
+    case ActionTypes.REQUEST_GET_ALL_REVIEW_COMMENTS_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_ADD_REVIEW_COMMENT_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_ADD_REVIEW_COMMENT_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_UPDATE_REVIEW_COMMENT_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_UPDATE_REVIEW_COMMENT_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_DELETE_REVIEW_COMMENT_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_DELETE_REVIEW_COMMENT_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_ADD_QUOTE_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_ADD_QUOTE_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_UPDATE_QUOTE_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_UPDATE_QUOTE_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_DELETE_QUOTE_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_DELETE_QUOTE_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_GET_ALL_QUOTE_COMMENTS_SUCCESS:
+      return {
+        ...state,
+        comments: action.payload,
+      };
+    case ActionTypes.REQUEST_GET_ALL_QUOTE_COMMENTS_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_ADD_QUOTE_COMMENT_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_ADD_QUOTE_COMMENT_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_UPDATE_QUOTE_COMMENT_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_UPDATE_QUOTE_COMMENT_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    case ActionTypes.REQUEST_DELETE_QUOTE_COMMENT_SUCCESS:
+      return {
+        ...state,
+      };
+    case ActionTypes.REQUEST_DELETE_QUOTE_COMMENT_FAILURE:
+      return {
+        ...state,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+}
+
+export default spaceReducer;
