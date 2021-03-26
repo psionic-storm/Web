135df2acf3ac34a9a74e3bdc25b543862daa4708
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-27 00:01:19 +0900

    feat : update handleClickBtns for category

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,17 +1,28 @@
+import { requestGetAllQuotes, requestGetAllReviews } from 'Modules/square/squareActions';
 import React, { useCallback } from 'react';
-import { useHistory } from 'react-router';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router';
 import * as S from './SideBarStyle';
 
 function SideBar() {
+  const dispatch = useDispatch();
+  const { search } = useLocation();
+  const category = search.split('=')[1];
   const history = useHistory();
 
   const handleClickReviewBtn = useCallback(() => {
     history.push('/square?category=reviews');
-  }, [history]);
+    if (category === 'reviews') {
+      dispatch(requestGetAllReviews());
+    }
+  }, [dispatch, history, category]);
 
   const handleClickQuoteBtn = useCallback(() => {
     history.push('/square?category=quotes');
-  }, [history]);
+    if (category === 'quotes') {
+      dispatch(requestGetAllQuotes());
+    }
+  }, [dispatch, history, category]);
 
   return (
     <S.Container>
