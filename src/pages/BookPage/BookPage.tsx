d22812134f68b7aebe013edebd9e82689ff9043d
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-28 04:09:44 +0900

    feat : add Post to the BookPage

--- src/pages/BookPage/BookPage.tsx
+++ src/pages/BookPage/BookPage.tsx
@@ -1,26 +1,41 @@
 import React, { useEffect, useMemo } from 'react';
 import * as S from './BookPageStyle';
 import { useDispatch, useSelector } from 'react-redux';
-import { requestGetBook as requestGetBookSpace } from 'Modules/space/spaceActions';
+import { requestGetBook as requestGetBookInSpace } from 'Modules/space/spaceActions';
 import { useParams } from 'react-router';
 import SideBar from 'Components/SideBar/SideBar';
 import Header from 'Components/Header/Header';
 import Post from 'Components/Post/Post';
+import { RootState } from 'Modules';
 
 function BookPage() {
+  const { book } = useSelector((state: RootState) => state.spaceReducer);
   const dispatch = useDispatch();
   const { spaceId, salonId, bookId } = useParams<{ spaceId?: string; salonId?: string; bookId: string }>();
 
   useEffect(() => {
     if (spaceId) {
-      dispatch(requestGetBookSpace({ spaceId: parseInt(spaceId), bookId: parseInt(bookId) }));
+      dispatch(requestGetBookInSpace({ spaceId: parseInt(spaceId), bookId: parseInt(bookId) }));
     }
-  });
+  }, [bookId, dispatch, spaceId]);
+
   return (
     <>
       <Header />
       <S.Container>
-        <S.PostWrapper></S.PostWrapper>
+        <S.ImgWrapper>
+          <S.Img src={book?.thumbnail} />
+        </S.ImgWrapper>
+        <S.PostWrapper>
+          <S.CategoryTitle>Review</S.CategoryTitle>
+          {book?.reviews?.map((review) => (
+            <Post key={review.id} type="review" review={review}></Post>
+          ))}
+          <S.CategoryTitle>Quote</S.CategoryTitle>
+          {book?.quotes?.map((quote) => (
+            <Post key={quote.id} type="quote" quote={quote}></Post>
+          ))}
+        </S.PostWrapper>
       </S.Container>
     </>
   );
--- src/pages/BookPage/BookPageStyle.ts
+++ src/pages/BookPage/BookPageStyle.ts
@@ -5,4 +5,24 @@ export const Container = styled.div`
   grid-template-columns: 1fr 1fr 1fr;
 `;
 
-export const PostWrapper = styled.div``;
+export const PostWrapper = styled.div`
+  margin-top: 20px;
+`;
+
+export const CategoryTitle = styled.div`
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const ImgWrapper = styled.div`
+  position: relative;
+`;
+
+export const Img = styled.img`
+  width: 140px;
+  float: right;
+  /* margin: 20px 40px 0 0; */
+  position: absolute;
+  top: 20px;
+  right: 40px;
+`;
