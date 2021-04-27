9d4263403acc6d196b567b178431b36b9323e4ad
jungcome7 <jungcome7@gmail.com>
2021-04-27 22:07:29 +0900

    test : add App test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   name: 'psionic-storm-test',
-  jest: {
-    verbose: true,
-  },
   moduleNameMapper: {
     '^Apis(.*)$': '<rootDir>/src/apis$1',
   },
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
+  afterEach(cleanup);
+
+  it('matches snapshot', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
   it('render', () => {
     render(<App />);
+  });
+
+  it('umm..', () => {
+    // const { getByText } = render(<App />);
+    // const asdf = getByText('asdf');
+    // expect(asdf).tobeInthe
   });
 
   it('1+1', () => {
