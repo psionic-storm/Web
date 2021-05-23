cbd701b42931f0083acef9e65544b405b7f7b3be
jungcome7 <jungcome7@gmail.com>
2021-05-24 01:26:13 +0900

    feat : add react-query QueryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,18 @@ import { AppProps } from 'next/app';
 
 import store from 'Modules';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function PsionicStormApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </QueryClientProvider>
     </>
   );
 }
