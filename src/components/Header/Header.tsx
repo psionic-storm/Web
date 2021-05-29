7ae18f173ef1374d649f9c80ae3ef7ebcf10cc79
jungcome7 <jungcome7@gmail.com>
2021-05-29 22:32:14 +0900

    feat : add Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import * as S from './HeaderStyle';
 function Header() {
   return (
     <S.Container>
+      <S.Logo>Psionic Storm</S.Logo>
       <Button>adsfadf</Button>
     </S.Container>
   );
--- src/components/Header/HeaderStyle.ts
+++ src/components/Header/HeaderStyle.ts
@@ -3,5 +3,14 @@ import PALETTE from 'Styles/color-variables';
 
 export const Container = styled.div`
   display: flex;
-  border-bottom: 1px solid ${PALETTE.GRAY[4]};
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+  border-bottom: 1px solid ${PALETTE.GRAY[2]};
+  padding: 0 30px;
+`;
+
+export const Logo = styled.div`
+  font-weight: bold;
+  font-size: 18px;
 `;
